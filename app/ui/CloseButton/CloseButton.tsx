267cc659cc7b50ef3f10983fde8e0f0399790178
Lucie <lye10@hawk.iit.edu>
2024-09-07 14:07:28 -0500

    Implemented dynamic close button component

---
+++ app/ui/CloseButton/CloseButton.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from './CloseButton.module.css';
+
+import Close from '@/public/buttons/close-button.png';
+import CloseIcon from '@/public/buttons/close-icon.png';
+
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  onClick: () => void;
+  isOpen: boolean;
+}
+
+export default function CloseButton({ onClick, isOpen }: ButtonProps) {
+  return (
+    <button className={styles.wrapper} onClick={onClick} >
+      <Image className={styles.image} src={Close} alt="Close Button" />
+      { isOpen && (
+        <Image
+          className={styles.icon}
+          src={CloseIcon}
+          alt="Close Icon"
+          onClick={onClick}
+        />
+      )}
+    </button>
+  );
+}
